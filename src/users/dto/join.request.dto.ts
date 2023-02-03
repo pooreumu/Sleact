274f639faea5492b182572df8f305c36e2b54d30
pooreumu <pooreum83@gmail.com>
2023-02-03 19:02:13 +0900

    ✨ add PickType

--- src/users/dto/join.request.dto.ts
+++ src/users/dto/join.request.dto.ts
@@ -1,24 +1,8 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { PickType } from '@nestjs/swagger';
+import { Users } from '../../entities/Users';
 
-export class JoinRequestDto {
-    @ApiProperty({
-        example: 'email@email.com',
-        description: '이메일',
-        required: true,
-    })
-    public email: string;
-
-    @ApiProperty({
-        example: 'pooreumu',
-        description: '닉네임',
-        required: true,
-    })
-    public nickname: string;
-
-    @ApiProperty({
-        example: 'weopf33q3ffj',
-        description: '비밀번호',
-        required: true,
-    })
-    public password: string;
-}
+export class JoinRequestDto extends PickType(Users, [
+    'email',
+    'nickname',
+    'password',
+] as const) {}
