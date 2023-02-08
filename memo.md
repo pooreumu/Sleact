90f7f09c64a16b971ea078770d945457f1e3e8a2
pooreumu <pooreum83@gmail.com>
2023-02-08 19:07:30 +0900

    ✨ typeorm 쿼리빌더

--- memo.md
+++ memo.md
@@ -1,4 +1,5 @@
-[순서 선후관계를 잘 알아둬야 한다] [1] 
+[순서 선후관계를 잘 알아둬야 한다] [1]
 
 [1]: https://docs.nestjs.com/faq/request-lifecycle
 
+상속받아서 쓸때 DI 잘 안되는 경우 있음 그럴 때 constructor에서 빼서 property로 만들어줌
--- src/workspaces/workspaces.controller.ts
+++ src/workspaces/workspaces.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Delete, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
+import { User } from 'src/common/decorator/user.decorator';
+import { Users } from 'src/entities/Users';
+import { CreateWorkspaceDto } from './dto/create-workspace.dto';
+import { WorkspacesService } from './workspaces.service';
 
 @Controller('api/workspaces')
 export class WorkspacesController {
+    constructor(private workspacesService: WorkspacesService) {}
     @Get()
-    getMyWorkspaces() {}
+    async getMyWorkspaces(@User() user: Users) {
+        return await this.workspacesService.findMyWorkspaces(user.id);
+    }
 
     @Post()
-    createWorkspace() {}
+    async createWorkspace(
+        @User() user: Users,
+        @Body() body: CreateWorkspaceDto,
+    ) {
+        return await this.workspacesService.createWorkspace(
+            body.workspace,
+            body.url,
+            user.id,
+        );
+    }
 
     @Get(':url/members')
     getAllMembersInWorkspaces() {}
